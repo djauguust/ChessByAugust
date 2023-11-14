e8f19e1c77acc10b3b598c8a7ef1edfd66de2697
AugustoBrito <dj.august.brito@gmail.com>
2023-11-13 21:21:25 -0300

    agregado las imagenes al tablero

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 
 export const Box = ({ position, value = null }) => {
+  const url = "/src/assets/images";
   let c = (position[0] + position[1]) % 2;
-  console.log(value);
+
+  const urlImagen = () => {
+    return `${url}/${value[0] === "b" ? `black` : `white`}/${value[1]}.png`;
+  };
+
   return (
     <button
       type="button"
       className={c === 1 ? "btn btn-light" : "btn btn-dark"}
       style={{ height: 72, width: 72 }}
     >
-      {value[1]}
+      {value[0] != "e" && (
+        <img
+          src={urlImagen()}
+          alt=""
+          width="50"
+          height="50"
+          className="d-inline-block align-text-top"
+        />
+      )}
     </button>
   );
 };
