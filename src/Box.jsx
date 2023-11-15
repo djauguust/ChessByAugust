c4c3c291507455a0f2b7f3bf536993e717165ca4
AugustoBrito <dj.august.brito@gmail.com>
2023-11-15 16:56:57 -0300

    ya tengo el tablero y las fichas, pero no respetan las reglas

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,27 +1,61 @@
-import React from "react";
+import React, { useContext } from "react";
+import { DataContext } from "./context/DataContext";
+import { changeScene } from "./functions/changeScene";
 
 export const Box = ({ position, value = null }) => {
   const url = "/src/assets/images";
   let c = (position[0] + position[1]) % 2;
+  const { raisedPiece, setRaisedPiece, sceneGame, setSceneGame } =
+    useContext(DataContext);
 
   const urlImagen = () => {
     return `${url}/${value[0] === "b" ? `black` : `white`}/${value[1]}.png`;
   };
 
+  const handleClick = () => {
+    if (!raisedPiece) {
+      if (value[0] != "e") {
+        let piece = {
+          color: `${value[0]}`,
+          piece: `${value[1]}`,
+          position: position,
+        };
+        setRaisedPiece(piece);
+      }
+    } else {
+      setSceneGame(changeScene(sceneGame, raisedPiece, position));
+      setRaisedPiece(null);
+    }
+  };
+
+  function isEqual(a, b) {
+    if (a == null) {
+      return false;
+    }
+    return a[0] === b[0] && a[1] === b[1];
+  }
+
   return (
     <button
       type="button"
-      className={c === 1 ? "btn btn-light" : "btn btn-dark"}
+      className={
+        isEqual(raisedPiece?.position, position)
+          ? "btn btn-warning"
+          : `${c === 1 ? "btn btn-light" : "btn btn-dark"}`
+      }
       style={{ height: 72, width: 72 }}
+      onClick={() => handleClick()}
     >
       {value[0] != "e" && (
-        <img
-          src={urlImagen()}
-          alt=""
-          width="50"
-          height="50"
-          className="d-inline-block align-text-top"
-        />
+        <a>
+          <img
+            src={urlImagen()}
+            alt=""
+            width="50"
+            height="50"
+            className="d-inline-block align-text-top"
+          />
+        </a>
       )}
     </button>
   );
--- src/Game.jsx
+++ src/Game.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 import { Board } from "./Board";
-import { getInitialGame } from "./getInitialGame";
+import { getInitialGame } from "./functions/getInitialGame";
 import { DataContext } from "./context/DataContext";
 
 export const Game = () => {
-  let initial = getInitialGame();
+  const [sceneGame, setSceneGame] = useState(getInitialGame());
 
   //DataContext
   const [raisedPiece, setRaisedPiece] = useState(null);
-  const dataMain = { raisedPiece, setRaisedPiece };
+  const dataMain = { raisedPiece, setRaisedPiece, sceneGame, setSceneGame };
 
   return (
     <DataContext.Provider value={dataMain}>
-      <Board scene={initial} />;
+      <Board scene={sceneGame} />;
     </DataContext.Provider>
   );
 };
